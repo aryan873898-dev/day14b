b4d7160381172d3a4af5848ba1f341f793b72810
aryan873898 <aryan873898@gmail.com>
2026-06-26 11:20:03 +0530

    FIRST PROJECT

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("First File");pull 
+const express = require("express")
+
+const app = express();
+
+let students = [
+    { id: 1, name: "SATYAM", city: "GORAKHPUR"},
+    { id: 2, name: "ALOK", city: "GKP"}
+];
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("API IS RUNNING");
+});
+
+app.get("/students",(req,res)=>{
+    res.json({
+        message:"ALL Atudents",
+        data: students
+    });
+});
+
+app.listen(3000,()=>{
+    console.log("Server Started");
+});
