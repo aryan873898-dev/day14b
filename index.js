91b43345c5690efe4ee65e9bbc25881bed34839a
aryan873898 <aryan873898@gmail.com>
2026-06-26 12:59:05 +0530

    deleted

--- index.js
+++ index.js
@@ -13,11 +13,58 @@ app.get("/",(req,res)=>{
     res.send("API IS RUNNING");
 });
 
+//Data 
+
 app.get("/students",(req,res)=>{
     res.json({
         message:"ALL Atudents",
         data: students
     });
+});
+
+app.post("/students",(req,res)=>{
+    const {id, name, city } = req.body;
+    const newStudent = { id, name, city };
+    students.push(newStudent);
+    res.json({
+        message:"Record Added",
+        student:newStudent,
+        data:students 
+    })
+});
+
+//Data Update
+app.put("/studens/:id",(req,res) => {
+    const { id } = req.params;
+
+    const student = students.find(s => s.id ==id);
+    if(!student)
+    {
+        return res.status(404).json({
+            message:"Student Not Found"
+        });
+    }
+    student.name = req.body.name;
+    student.city = req.body.city;
+
+    res.json({
+        message:"Record updated",
+        student
+    });
+})
+
+app.delete("/students/:id", (req, res)=> {
+    const id = req.params.id;
+    const student =students.find(s => s.id == id);
+    if (!student) {
+        return res.status(404).json({message: "Invalid ID"});
+
+    }
+    students = students.filter(s => s.id !=id);
+    res.json({
+        message:"Record Deleted",
+        students
+    })
 });
 
 app.listen(3000,()=>{
